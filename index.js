0248c490f7d047e82a8812dce7fa989da949772f
demiro <damir21@hotmail.com>
2021-03-13 20:17:46 +0000

    fixed reference to templates path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const templatePath = __dirname + "/template";
 
 if (process.argv.length < 3) {
   console.log("Please enter component name!");
@@ -13,29 +14,33 @@ if (process.argv.length < 3) {
     if (err) {
       console.log(err.message);
     } else {
-      fs.createReadStream("template/component.html").pipe(
+      fs.createReadStream(`${templatePath}/component.html`).pipe(
         fs.createWriteStream(path + ".html")
       );
 
-      fs.createReadStream("template/component.scss").pipe(
+      fs.createReadStream(`${templatePath}/component.scss`).pipe(
         fs.createWriteStream(path + ".scss")
       );
 
-      fs.readFile("template/component.vue", "utf8", function (err, content) {
-        content = content.replace(/{componentName}/g, componentName);
-        content = content.replace(
-          "{componentNameUpperCase}",
-          componentName.charAt(0).toUpperCase() + componentName.slice(1)
-        );
+      fs.readFile(
+        `${templatePath}/component.vue`,
+        "utf8",
+        function (err, content) {
+          content = content.replace(/{componentName}/g, componentName);
+          content = content.replace(
+            "{componentNameUpperCase}",
+            componentName.charAt(0).toUpperCase() + componentName.slice(1)
+          );
 
-        fs.writeFile(path + ".vue", content, "utf8", function (err) {
-          if (err) {
-            console.log(err.message);
-          } else {
-            console.log("Component created!");
-          }
-        });
-      });
+          fs.writeFile(path + ".vue", content, "utf8", function (err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log("Component created!");
+            }
+          });
+        }
+      );
     }
   });
 }
