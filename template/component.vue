67b0f0a19bae99021c20a227c43371b62b2e3fe5
demiro <damir21@hotmail.com>
2021-03-13 09:54:24 +0000

    basic functionality

---
+++ template/component.vue
@@ -0,0 +1,19 @@
+<template src="./{componentName}.html"></template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+import "./{componentName}.scss";
+export default defineComponent({
+  name: "{componentNameUpperCase}",
+  props: {},
+  data() {
+    return {};
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+
+  methods: {},
+});
+</script>
