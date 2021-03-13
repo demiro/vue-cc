695db3e1bf02e1d6b56c153114c92a4603caedea
demiro <damir21@hotmail.com>
2021-03-13 20:56:47 +0000

    proper directories, cleanup

--- index.js
+++ index.js
@@ -7,40 +7,37 @@ if (process.argv.length < 3) {
   console.log("Please enter component name!");
 } else {
   const componentName = process.argv[2];
-  console.log(componentName);
-  const path = "./" + componentName + "/" + componentName;
 
-  fs.mkdir("./src/components/" + componentName, function (err) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      fs.createReadStream(`${templatePath}/component.html`).pipe(
-        fs.createWriteStream(path + ".html")
-      );
+  const path = "./src/components/" + componentName;
 
-      fs.createReadStream(`${templatePath}/component.scss`).pipe(
-        fs.createWriteStream(path + ".scss")
-      );
+  fs.existsSync(path) || fs.mkdirSync(path);
 
-      fs.readFile(
-        `${templatePath}/component.vue`,
-        "utf8",
-        function (err, content) {
-          content = content.replace(/{componentName}/g, componentName);
-          content = content.replace(
-            "{componentNameUpperCase}",
-            componentName.charAt(0).toUpperCase() + componentName.slice(1)
-          );
+  fs.createReadStream(`${templatePath}/component.html`).pipe(
+    fs.createWriteStream(path + "/" + componentName + ".html")
+  );
 
-          fs.writeFile(path + ".vue", content, "utf8", function (err) {
-            if (err) {
-              console.log(err.message);
-            } else {
-              console.log("Component created!");
-            }
-          });
+  fs.createReadStream(`${templatePath}/component.scss`).pipe(
+    fs.createWriteStream(path + "/" + componentName + ".scss")
+  );
+
+  fs.readFile(`${templatePath}/component.vue`, "utf8", function (err, content) {
+    content = content.replace(/{componentName}/g, componentName);
+    content = content.replace(
+      "{componentNameUpperCase}",
+      componentName.charAt(0).toUpperCase() + componentName.slice(1)
+    );
+
+    fs.writeFile(
+      path + "/" + componentName + ".vue",
+      content,
+      "utf8",
+      function (err) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log("Component created!");
         }
-      );
-    }
+      }
+    );
   });
 }
